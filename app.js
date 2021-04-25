95391e31ba08420bf25844db44d9a902557b0cfb
Leonardo Zeaiter <leonardozeaiter@gmail.com>
2021-04-25 08:55:08 +0300

    Nb9

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get("/", (req, res, next) => {
     res.send("Welcome home babe.");
 });
 
-app.listen(port.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("App started and listening on port 3000.");
-    console.log(port.env.PORT);
+    console.log(process.env.PORT);
 });
